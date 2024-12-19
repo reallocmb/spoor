199e327b6e082ac2d9640ce0817fa3449151e16c
reallocmb <realloc.mb@gmail.com>
2024-12-19 23:26:10 +0100

    alphabet sort and bug tasklist

--- src/s_sort.c
+++ src/s_sort.c
@@ -1,5 +1,27 @@
 #include"spoor.h"
 
+s32 alphabet_compare(char *buffer0, char *buffer1)
+{
+    s32 diff = 0;
+    u32 i;
+
+    for (i = 0; !(buffer0[i] == 0 || buffer1[i] == 0); i++)
+    {
+        diff = buffer0[i] - buffer1[i];
+        if (diff < 0)
+            return -1;
+        if (diff > 0)
+            return 1;
+    }
+
+    if (strlen(buffer0) > strlen(buffer1))
+        return 1;
+    else
+        return -1;
+
+    return 0;
+}
+
 s32 sort_func(const void *data0, const void *data1)
 {
     SpoorObject *spoor_object0 = (SpoorObject *)data0;
@@ -10,6 +32,8 @@ s32 sort_func(const void *data0, const void *data1)
         result = time_compare(&spoor_object0->schedule.start, &spoor_object1->schedule.start);
     if (result == 0)
         result = spoor_object1->priority - spoor_object0->priority;
+    if (result == 0)
+        result = alphabet_compare(spoor_object0->title, spoor_object1->title);
 
     return result;
 }
